1bac3ffb19d0e436fa2ab559ab2bc8f1d51d2354
AdityaBhargavaReddy <bhargavareddy2005@gmail.com>
2025-01-02 10:47:28 +0530

    2

--- src/App.js
+++ src/App.js
@@ -6,6 +6,81 @@ class App extends Component {
     return (
       <div>
         <Header/>
+        <p>
+        corded. Willi composed an opera, Schlafes Bruder, for the Opernhaus Zürich.
+
+Life
+Willi was born in Bludenz, Vorarlberg. He studied music pedagogy and theology at the University of Innsbruck, and simultaneously bassoon and piano at the Innsbruck conservatory.[1] From 1983, he studied composition with Helmut Eder at the Salzburg Mozarteum, then with Boguslaw Schaeffer.[2] He lives in Sankt Anton im Montafon.[1]
+
+Work
+Willi's work comprises an opera, orchestral works, and chamber music, including compositions for one player. Willi received a commission from the Salzburg Festival and the Cleveland Orchestra in 1991 for Konzert für Orchester. The premiere was conducted by Christoph von Dohnányi.[1] In 1994/95, the Opernhaus Zürich commissioned the opera Schlafes Bruder on the occasion of Austria's millennial, with a libretto by Robert Schneider, who had written the novel on which it is based, Schlafes Bruder. It was premiered in 1996.[1] In 1997/98, Willi composed Begegnung für Orchester on a commission by the Wiener Philharmoniker for the orchestra's 150th anniversary.[1]
+
+He composed Montafon, a concerto cycle containing the works Eirene, a trumpet concerto; ...geraume Zeit..., a concerto for flute, oboe and orchestra; ego eimi, a clarinet concerto; and Äon, a horn concerto.[1] A revised version of Schlafes Bruder was produced at the Stadttheater Klagenfurt in March 2008.[1]
+
+Willi's works were performed in Carnegie Hall in New York City, Tokyo, the Royal Albert Hall in London, and at the Philharmonie Berlin, among others. They were played by the Berlin Philharmonic, Wiener Philharmoniker, Cleveland Orchestra, Philadelphia Orchestra, Pittsburgh Symphony Orchestra and New Japan Philharmonic, conducted by Claudio Abbado, Gustavo Dudamel, Manfred Honeck, Riccardo Muti and Seiji Ozawa.[1]
+
+His works were recorded: the WERGO label published orchestral works including Eirene, Räume, Rondino after his opera Schlafes Bruder, geraume Zeit and Begegnung, and the Japanese label Camerata Tokyo released a CD with chamber music from 1984 to 2005.[1]
+
+Awards
+Willi received scholarships and awards, and was composer in residence:[1]
+
+1985, 1989: Austrian state scholarship for composers[3]
+1987: Prize at the competition for string quartets of the Wiener Konzerthausgesellschaft (for his Streichquartett 1986 premiered by the Arditti Quartet)
+1989: Erste-Bank-Kompositionspreis [de]
+1990: Rolf-Liebermann-Stipendium für Opernkompositionen[3]
+1991: Ernst von Siemens Composers' Prize[3]
+1997: Österreichisches Ehrenzeichen für Wissenschaft und Kunst[3]
+1998: Großes Verdienstzeichen des Landes Vorarlberg[3]
+1992: Composer in residence at the Salzburg Festival
+1996–1998: Composer in residence with the Camerata Academia
+2002/2003: Composer in residence with the Gesellschaft der Musikfreunde of the Wiener Musikverein and the Wiener Concert-Verein[3]
+2007: Composer in residence at the Pacific Music Festival in Sapporo
+2008: Composer in residence at the International Summer Music Academy & Festival in Kusatsu
+Works
+Willi's works were published by Musikverlag Doblinger [de][2] and Schott Music, including:[1]
+
+Stage
+Schlafes Bruder, opera in a prologue, eight scenes and an epilogue (1994–1996, rev. 2006)[4]
+Orchestral and concertante
+Der Froschmäusekrieg (UA 1989) after the epic
+Für 16, small chamber concerto (1990)
+Konzert (1991/92)
+Flötenkonzert (1993)
+Begegnung (1997/98)
+Rondino (1999/2000)
+Eirene, trumpet concerto (2001)
+...geraume Zeit..., concerto for flute, oboe and orchestra (2002/03)
+ego eimi, clarinet concerto (2005/06)
+Äon, horn concerto (2007)
+ABBA-MA (Echo of Peace), for choir and orchestra – German text based on the Lord's Prayer (2011)
+Sacrosanto, violin concerto (2011/12)
+Chamber music
+Stück for clarinet solo (1985), premiered by Alois Brandhofer[5]
+Stück for flute solo (1985/86)
+Trio for violin, horn and piano (1992)
+Kairos im Kronos 1756/1956 for violin, viola and cello (2005)
+References
+ "Herbert Willi". Schott Music. Retrieved 30 December 2019.
+ "Willi, Herbert". Musikverlag Doblinger (in German). Archived from the original on 23 February 2021. Retrieved 30 December 2019.
+ Harten, Uwe (2019). "Willi, Herbert". Oesterreichisches Musiklexikon online (in German). Österreichische Akademie der Wissenschaften. Retrieved 31 December 2019.
+ Umbach, Klaus (22 April 1996). "Wurzelsepp im Wunderland". Der Spiegel (in German). Hamburg. Retrieved 30 December 2019.
+ "Stück für Klarinette solo". Music Austria. Retrieved 3 January 2015.
+External links
+Literature by and about Herbert Willi in the German National Library catalogue
+Klavier im Stau at IMDb (TV portrait)
+Grossmann, Midou. "Montafon-Zyklus' auf luftiger Höhe?". klassik.com (in German).
+"Herbert Willi". Musikdokumentation Arlberg.
+Authority control databases Edit this at Wikidata
+International	
+ISNIVIAFFASTWorldCat
+National	
+GermanyUnited StatesFranceBnF dataJapanNorwayIsrael
+Academics	
+CiNii
+Artists	
+MusicBrainz
+Categories: Austrian male classical composers20th-century Austrian classical composersRecipients of the Austrian Decoration for Science and ArtUniversity of Innsbruck alumniMozarteum University Salzburg alumniLiving people1956 birthsPeople from Bludenz20th-century Austrian male musicians
+        </p>
       </div>
     );
   }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,18 @@
 import { Component } from "react";
+import './app.css'
 
 class Header extends Component {
     render(){
         return(
-            
+            <div>
+                <nav className="navigation-bar">
+                    <div className="name-section">
+                        <img className="logo" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="Logo"/>
+                        <h1 className="name"><span className="span">A</span>di</h1>
+                        <p className="role">Full Stack Developer</p>
+                    </div>
+                </nav>
+            </div>
         )
     }
 }
